b820983ca6bde8935442edd2f430d769de99fae2
Sara Bilmes <saraesrig@gmail.com>
2017-06-23 15:03:29 +0300

    saved messages.js

---
+++ app/scripts/services/Messages.js
@@ -0,0 +1,19 @@
+(function() {
+  function Message($firebaseArray) {
+    var Message = {};
+    var ref = firebase.database().ref().child("messages");
+    var messages = $firebaseArray(ref);
+
+    Message.getByRoomId = function(roomId) {
+        // Filter the messages by their room ID.
+      messages.orderByChild('roomID').equalTo('roomId');
+
+  };
+
+    return Message;
+  }
+
+  angular
+    .module('blocChat')
+    .factory('Message', ['$firebaseArray', Message]);
+})();
