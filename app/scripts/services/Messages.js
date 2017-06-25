6b613459061684863746118470f5299562c04da6
Sara Bilmes <saraesrig@gmail.com>
2017-06-25 20:55:22 +0300

    list messages attempt 5

--- app/scripts/services/Messages.js
+++ app/scripts/services/Messages.js
@@ -4,13 +4,17 @@
     var ref = firebase.database().ref().child("messages");
     var messages = $firebaseArray(ref);
 
+
     Message.getByRoomId = function(roomId) {
         // Filter the messages by their room ID.
-     messages.orderByChild('roomID').equalTo(roomId);
+    return  messages.orderByChild('roomId').equalTo(roomId);
+
 
   };
 
     return Message;
+
+
   }
 
   angular
